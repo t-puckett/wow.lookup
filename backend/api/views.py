84d3706200632071a7f9f36a4228b1da4121a153
Fi-Ren-Ze <arflamion@gmail.com>
2026-03-08 23:10:39 -0400

    Added example endpoint logic and a simple user password for jwt retrieval

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Article
+from .serializers import ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,8 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
     'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
